d3ef847d7f4df809856a96caffc369862b970c37
CandaceRossi <50240571+CandaceRossi@users.noreply.github.com>
2019-06-14 14:14:36 -0400

    finished the challenge!

--- challenges/functions.js
+++ challenges/functions.js
@@ -7,41 +7,31 @@
  * In the body of the function return the callback with the two parameters that you created
  */
 function consume(x, y, cb) {
-    cb(x) + cb(y);
+    console.log(cb(x, y));
+
 }
-consume(2, 3, function(itis) {
-    console.log(itis);
-});
 
 /* Step 2: Create several functions to callback with consume();
  * Create a function named add that returns the sum of two numbers
  * Create a function named multiply that returns the product of two numbers 
  * Create a function named greeting that accepts a first and last name and returns "Hello first-name last-name, nice to meet you!"
  */
-const sumNums = (x, y, cb) => {
-    cb(x + y);
+let add = (x, y) => {
+    return (x + y);
 }
-sumNums(2, 3, function(sum) {
-    console.log(sum);
-});
 
-const multiplyNums = (x, y, cb) => {
-    cb(x * y);
+let multiply = (x, y) => {
+    return (x * y);
 }
-multiplyNums(2, 3, function(product) {
-    console.log(product);
-});
 
-const greetings = (x, y, cb) => {
-    cb(`Hello ${x} ${y}, nice to meet you!`);
-};
-greetings('candace', 'rossi', function(greeting) {
-    console.log(greeting);
-});
+let greeting = (x, y) => {
+    return (`Hello ${x} ${y}, nice to meet you!`);
+}
+
 /* Step 3: Check your work by un-commenting the following calls to consume(): */
-// consume(2,2,add); // 4
-// consume(10,16,multiply); // 160
-// consume("Mary","Poppins", greeting); // Hello Mary Poppins, nice to meet you!
+consume(2, 2, add); // 4
+consume(10, 16, multiply); // 160
+consume("Mary", "Poppins", greeting); // Hello Mary Poppins, nice to meet you!
 
 
 // ==== Closures ==== 
@@ -49,6 +39,7 @@ greetings('candace', 'rossi', function(greeting) {
 // Explain in your own words why `nestedfunction()` can access the variable `internal`.
 
 // Explanation: 
+
 //Nestedfunction() can access the variables inside of the scope of the object because 
 //it is can reach outward as a function in its object. 
 
